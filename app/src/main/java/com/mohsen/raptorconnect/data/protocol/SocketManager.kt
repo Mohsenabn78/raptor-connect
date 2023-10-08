45997d06d942de1a232b4d661996c47adc6c8905
mohesen abedini <m.abedini@email.com>
2023-10-08 15:34:58 +0330

    add retry policy

--- app/src/main/java/com/mohsen/raptorconnect/data/protocol/SocketManager.kt
+++ app/src/main/java/com/mohsen/raptorconnect/data/protocol/SocketManager.kt
@@ -12,20 +12,24 @@ object SocketManager {
     init {
         val options = IO.Options()
         options.forceNew = true
+        options.reconnection = true
+        options.reconnectionAttempts = 10
+        options.reconnectionDelay = 5000
+        options.timeout = 5000
         mSocket = IO.socket(SERVER_URL, options)
     }
 
-    fun connect() {
-        mSocket.connect()
-    }
+    fun connect() = mSocket.connect()
 
-    fun disconnect() {
-        mSocket.disconnect()
-    }
 
-    fun sendMessage(message: String) {
-        mSocket.emit("message", message)
-    }
+    fun disconnect() = mSocket.disconnect()
+
+
+    fun isActive() = mSocket.isActive
+
+
+    fun sendMessage(message: String) = mSocket.emit("message", message)
+
 
     fun setOnMessageListener(listener: (String) -> Unit) {
         mSocket.on("message") { args ->
